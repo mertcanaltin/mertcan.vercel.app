200de0c6ff2a7bee4d59f4e87d4a422e3632930a
Mert Can Altin <mert.altin@trendyol.com>
2024-06-22 13:11:59 +0300

    feat: Blog infrastructure for Mert can altin

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,5 +1,5 @@
 export const githubImage = 'https://github.com/mertcanaltin.png'
-export const twitterId = '1589638196'
+export const twitterId = '991578763073900544'
 export const twitterUsername = 'mecaltin'
 export const twitterUrl = `https://twitter.com/${twitterUsername}`
 export const rssUrl = 'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.mertcan.vercel.app%2Frss%2F'
