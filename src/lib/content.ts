c7496daccd64cdb2dc4c944f881af20e1d37c83f
Mert Can Altin <mert.altin@trendyol.com>
2024-06-23 17:29:57 +0300

    fix: update rss link

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -2,7 +2,7 @@ export const githubImage = 'https://github.com/mertcanaltin.png'
 export const twitterId = '991578763073900544'
 export const twitterUsername = 'mecaltin'
 export const twitterUrl = `https://twitter.com/${twitterUsername}`
-export const rssUrl = 'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.mertcan.vercel.app%2Frss%2F'
+export const rssUrl = 'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fmertcan.vercel.app%2Frss'
 export const websiteTitle = `Mert Can Altin's blog`
 export const websiteDescription =
   'Mert Can Altin is a software engineer who writes about software engineering, programming, and technology. His research and work is focused on software performance.'
